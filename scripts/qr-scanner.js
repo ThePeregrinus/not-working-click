9492885146907e715c06547e1eb21f731e365d3b
Mark <30576056+ThePeregrinus@users.noreply.github.com>
2024-11-29 12:25:46 +0300

    Add files via upload

--- scripts/qr-scanner.js
+++ scripts/qr-scanner.js
@@ -1,6 +1,6 @@
 function initQR() {
   async function prep() {
-    let h = screen.height;
+    let h = window.innerHeight;
     let w = screen.width;
     const aspectRatio = w / h;
     let qrboxFunction = function (viewfinderWidth, viewfinderHeight) {
