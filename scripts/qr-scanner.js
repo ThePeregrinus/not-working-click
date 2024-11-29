64244a9daac649460f3e6e64c16b653f1d0f3205
Mark <30576056+ThePeregrinus@users.noreply.github.com>
2024-11-29 21:09:51 +0300

    Add files via upload

--- scripts/qr-scanner.js
+++ scripts/qr-scanner.js
@@ -1,58 +1,62 @@
 function initQR() {
-  const h = innerHeight;
-  const w = innerWidth;
+  h = null;
+  w = null;
+  setTimeout(() => {
+    h = innerHeight;
+    w = innerWidth;
 
-  // const h = document.documentElement.clientHeight;
-  // const w = document.documentElement.clientWidth;
-  console.log(`h=${h}`);
-  console.log(`w=${w}`);
+    // const h = document.documentElement.clientHeight;
+    // const w = document.documentElement.clientWidth;
+    console.log(`h=${h}`);
+    console.log(`w=${w}`);
 
-  async function prep() {
-    const aspectRatio = w >= h ? w / h : h / w;
-    let qrboxFunction = function (viewfinderWidth, viewfinderHeight) {
-      let minEdgePercentage = 0.6; // percentage for edge
-      let minEdgeSize = Math.min(w, h);
-      let qrboxSize = Math.floor(minEdgeSize * minEdgePercentage);
-      return {
-        width: qrboxSize * 1.4,
-        height: qrboxSize,
+    async function prep() {
+      const aspectRatio = w >= h ? w / h : h / w;
+      let qrboxFunction = function (viewfinderWidth, viewfinderHeight) {
+        let minEdgePercentage = 0.6; // percentage for edge
+        let minEdgeSize = Math.min(w, h);
+        let qrboxSize = Math.floor(minEdgeSize * minEdgePercentage);
+        return {
+          width: qrboxSize * 1.4,
+          height: qrboxSize,
+        };
       };
-    };
 
-    const formatsToSupport = [Html5QrcodeSupportedFormats.QR_CODE];
+      const formatsToSupport = [Html5QrcodeSupportedFormats.QR_CODE];
 
-    const scanner = new Html5Qrcode("qr-reader");
-    const config = {
-      fps: 10, // frame per seconds for qr code scanning
-      qrbox: qrboxFunction,
-      aspectRatio: aspectRatio,
-      formatsToSupport,
-    };
+      const scanner = new Html5Qrcode("qr-reader");
+      const config = {
+        fps: 10, // frame per seconds for qr code scanning
+        qrbox: qrboxFunction,
+        aspectRatio: aspectRatio,
+        formatsToSupport,
+      };
 
-    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-      window.location.href = `ar.html?vin=` + decodedText;
-    };
+      const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+        window.location.href = `ar.html?vin=` + decodedText;
+      };
 
-    scanner
-      .start(
-        { facingMode: { exact: "environment" } },
-        config,
-        qrCodeSuccessCallback
-      )
-      .catch(() => {
-        scanner.start(
-          { facingMode: { exact: "user" } },
+      scanner
+        .start(
+          { facingMode: { exact: "environment" } },
           config,
           qrCodeSuccessCallback
-        );
-      })
-      .then(() => {
-        document
-          .getElementById("qr-reader")
-          .scrollIntoView({ block: "center" });
-      });
-  }
-  prep();
+        )
+        .catch(() => {
+          scanner.start(
+            { facingMode: { exact: "user" } },
+            config,
+            qrCodeSuccessCallback
+          );
+        })
+        .then(() => {
+          document
+            .getElementById("qr-reader")
+            .scrollIntoView({ block: "center" });
+        });
+    }
+    prep();
+  }, 3000);
 }
 
 function docReady(fn) {
